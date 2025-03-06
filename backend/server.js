01aab631c1a467cb279acf73314486da346c8f9c
AbdulSamadNizamani <168513302+AbdulSamadNizamani@users.noreply.github.com>
2025-03-06 22:15:13 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import app from "./index.js";
 import colors from 'colors'
-// import Connect from "./db/Connect.js";
 import Connect from "./db/Connect.js";
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
 app.listen(process.env.PORT,async()=>{
   await Connect();
     console.log(`My app is running on ${process.env.PORT}`.yellow)
